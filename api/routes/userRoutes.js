e69210d8a2dfbda04e27b494f5c4cda4421c0598
Younes <yoones13844@gmail.com>
2025-10-23 16:12:36 -0600

    added login signup and getAll and createUser routes

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,3 +1,12 @@
 const app = require('express');
+const { login, signup } = require('../controllers/authController');
+const { getAllUsers, createUser } = require('../controllers/userController');
 
 const router = app.Router();
+
+router.post('/login', login);
+router.post('/signup', signup);
+
+router.route('/').get(getAllUsers).post(createUser);
+
+module.exports = router;
